1a32a511b8efd196c515ad165b08646e5e7bd9d7
Playbird12 <83607406+Playbird12@users.noreply.github.com>
2021-05-08 20:09:56 -0500

    sruff

--- index.js
+++ index.js
@@ -10,21 +10,32 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+	// when a message is sent this code is executed
 	socket.on('chat message', (msg) => {
 		console.log('message: ' + msg);
+
+		switch (msg) {
+		case '!nick':
+			break;
+		default:
+			break;
+		}
+
 		io.emit('chat message', msg);
 	});
 	console.log('a user connected');
-	io.emit('chat message', 'a user connected');
-	io.emit('chat message', 'please enter a nickname with the !nick command');
 
+	// detects when a user disconnects
 	socket.on('disconnect', () => {
 		io.emit('chat message', 'a user disconnected');
 		console.log('a user disconnected');
 	});
 
+	// what is sent when a user joins
+	io.emit('chat message', 'a user connected');
 });
 
+// defines which port it's listening on
 server.listen(3000, () => {
 	console.log('listening on *:3000');
 });
